6b028bddcd88052ee2ebd884d773ffecb0b00433
DLLM <DLLM@gft.com>
2022-10-08 03:09:00 -0300

    codigo rodando bonitasso, parando na aula 11

--- src/pages/feed/index.js
+++ src/pages/feed/index.js
@@ -1,21 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#f5f5f5',
-        marginTop: 32,
-        paddingHorizontal: 24,
-    },
-    title: {
-        fontSize: 24,
-        textAlign: 'center',
-        margin: 10,
-    },
-});
+import { styles } from './styles';
 
 export default function App() {
     return (
---
+++ src/pages/feed/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#f5f5f5',
+        marginTop: 32,
+        paddingHorizontal: 24,
+    },
+    title: {
+        fontSize: 24,
+        textAlign: 'center',
+        margin: 10,
+    },
+});
